83331a32fad831b78cd816b7696bc9e67c722423
JeelsBoobz <38210868+JeelsBoobz@users.noreply.github.com>
2018-11-19 22:27:51 +0700

    Update zippyshare.sh

--- zippyshare.sh
+++ zippyshare.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # @Description: zippyshare.com file download script
 # @Author: Live2x
+# @Revode: JeelsBoobz
 # @URL: https://github.com/img2tab/zippyshare
-# @Version: 201809100002
-# @Date: 2018-06-27
+# @Version: 201811270001
+# @Date: 2018-11-19
 # @Usage: ./zippyshare.sh url
 
 urldecode() {
@@ -38,7 +39,7 @@ function zippydownload()
         --keep-session-cookies \
         --save-cookies="${cookiefile}" \
         --quiet
-        filename="$( cat "${infofile}" | grep "/d/" | cut -d'/' -f5 | cut -d'"' -f1 | grep -o "[^ ]\+\(\+[^ ]\+\)*" )"
+        filename="$( grep 'og:title' "${infofile}" | grep -Po '(?<=<meta property="og:title" content=").*?(?= " />)')"
     done
 
     if [ "${retry}" -ge 10 ]
@@ -59,10 +60,10 @@ function zippydownload()
     if [ -f "${infofile}" ]
     then
         # Get url algorithm
-        dlbutton="$( grep 'getElementById..dlbutton...href' "${infofile}" | grep -oE '\([0-9].*\)' )"
+        dlbutton="$( grep 'poster=true&amp;time=' "${infofile}" | grep -Po '(?<=poster=true&amp;time=).*?(?=">)')"
         if [ -n "${dlbutton}" ]
         then
-           algorithm="${dlbutton}"
+           algorithm=$((${dlbutton:(-3)} + 1 + 2 + 3 + 4 + 5 / 5 ))
         else
            echo "could not get zippyshare url algorithm"
            exit 1
@@ -86,10 +87,10 @@ function zippydownload()
     # Set browser agent
     agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36"
 
-    echo "$(urldecode ${filename})"
+    echo "$( urldecode ${filename})"
 
     # Start download file
-    wget -c -O "$(urldecode ${filename})" "${dl}" \
+    wget -c -O "$( urldecode ${filename})" "${dl}" \
     -q --show-progress \
     --referer="${ref}" \
     --cookies=off --header "Cookie: JSESSIONID=${jsessionid}" \
